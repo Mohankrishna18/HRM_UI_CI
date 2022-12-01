35f5855edbec833a15909518d90af2a4c5a59cf7
mohan <Mohankrishna.madanapu@arshaa.com>
2022-12-01 16:39:47 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Col, Row, Container } from "react-bootstrap";
-import Routes from "./routes";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import {toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-toast.configure()
+// import { Col, Row, Container } from "react-bootstrap";
+// import Routes from "./routes";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css";
+// import {toast} from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// toast.configure()
 
 const App = () => {
   return (
 
-    <div className="AppComponent">
+    <div >
       {/* <Container-fluid>
         <Row>
           <Col>
